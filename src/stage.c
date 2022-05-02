fe0d9bd706c2bf22c46b20f83928fd28df5ca9db
puzzlehead <dylanlim12698@gmail.com>
2022-05-02 14:13:29 +0800

    Implement enemy retaliation

--- src/stage.c
+++ src/stage.c
@@ -15,11 +15,18 @@ static void doFighters(void);
 static void spawnEnemies(void);
 static void drawFighters(void);
 static int bulletHitFighter(struct Entity *b);
+static void resetStage(void);
+static void doEnemies(void);
+static void fireAlienBullets(struct Entity *e);
+static void clipPlayer(void);
 
 static SDL_Texture *bulletTexture = NULL;
 static SDL_Texture *enemyTexture = NULL;
+static SDL_Texture *alienBulletTexture = NULL;
+static SDL_Texture *playerTexture = NULL;
 
 static int enemySpawnTimer = 0;
+static int stageResetTimer = 0;
 
 static struct Entity *player;
 
@@ -31,10 +38,12 @@ void initStage(void) {
     stage.fighterTail = &stage.fighterHead;
     stage.bulletTail = &stage.bulletHead;
 
-    initPlayer();
-
+    playerTexture = loadTexture("gfx/player.png");
     bulletTexture = loadTexture("gfx/playerBullet.png");
     enemyTexture = loadTexture("gfx/enemy.png");
+    alienBulletTexture = loadTexture("gfx/alienBullet.png");
+
+    resetStage();
 }
 
 static void initPlayer() {
@@ -43,14 +52,48 @@ static void initPlayer() {
     stage.fighterTail->next = player;
     stage.fighterTail = player;
 
+    player->health = 1;
     player->x = 100;
     player->y = 100;
-    player->texture = loadTexture("gfx/player.png");
+    player->texture = playerTexture;
     player->side = SIDE_PLAYER;
+
     SDL_QueryTexture(player->texture, NULL, NULL, &player->w, &player->h);
 }
 
+static void resetStage(void) {
+    struct Entity *e;
+
+    while (stage.fighterHead.next) {
+        e = stage.fighterHead.next;
+        stage.fighterHead.next = e->next;
+        free(e);
+    }
+
+    while (stage.bulletHead.next) {
+        e = stage.bulletHead.next;
+        stage.bulletHead.next = e->next;
+        free(e);
+    }
+
+    memset(&stage, 0, sizeof(Stage));
+    stage.fighterTail = &stage.fighterHead;
+    stage.bulletTail = &stage.bulletHead;
+
+    initPlayer();
+
+    enemySpawnTimer = 0;
+
+    stageResetTimer = FPS * 2;
+}
+
 static void logic(void) {
+    clipPlayer();
+
+    if (player == NULL && --stageResetTimer <= 0) {
+        resetStage();
+    }
+
     doPlayer();
 
     doBullets();
@@ -58,9 +101,15 @@ static void logic(void) {
     doFighters();
 
     spawnEnemies();
+
+    doEnemies();
 }
 
 static void doPlayer() {
+    if (player == NULL) {
+        return;
+    }
+
     player->dx = player->dy = 0;
 
     if (player->reload > 0)
@@ -127,7 +176,7 @@ static void doBullets(void) {
         b->x += b->dx;
         b->y += b->dy;
 
-        if (bulletHitFighter(b) || b->x > SCREEN_WIDTH) {
+        if (bulletHitFighter(b) || b->x < -b->w || b ->y < -b->h || b->x > SCREEN_WIDTH || b->y > SCREEN_HEIGHT) {
             if (b == stage.bulletTail) {
                 stage.bulletTail = prev;
             }
@@ -165,8 +214,20 @@ static void doFighters(void) {
         e->x += e->dx;
         e->y += e->dy;
 
-        if (e != player && (e->x < -e->w || e->health == 0)) {
-            if (e == stage.fighterTail) {
+        if (e != player && e->x < -e->w)
+        {
+            e->health = 0;
+        }
+
+        if (e->health == 0)
+        {
+            if (e == player)
+            {
+                player = NULL;
+            }
+
+            if (e == stage.fighterTail)
+            {
                 stage.fighterTail = prev;
             }
 
@@ -211,6 +272,7 @@ static void spawnEnemies(void) {
         SDL_QueryTexture(enemy->texture, NULL, NULL, &enemy->w, &enemy->h);
 
         enemy->dx = -(2 + (rand() % 4));
+        enemy->reload = FPS * (1 + (rand() % 3));
 
         enemySpawnTimer = 30  + (rand() % 60);
     }
@@ -221,5 +283,68 @@ static void drawFighters(void) {
 
     for (e = stage.fighterHead.next; e != NULL; e = e->next) {
         blit(e->texture, e->x, e->y);
+    }
+}
+static void doEnemies(void) {
+    struct Entity *e;
+
+    for (e = stage.fighterHead.next; e != NULL; e = e->next) {
+        if (e != player && player != NULL && --e->reload <= 0) {
+            fireAlienBullets(e);
+        }
+    }
+}
+
+static void fireAlienBullets(struct Entity *e) {
+    struct Entity *bullet;
+
+    bullet = malloc(sizeof(struct Entity));
+    memset(bullet, 0, sizeof(struct Entity));
+    stage.bulletTail->next = bullet;
+    stage.bulletTail = bullet;
+
+    bullet->x = e->x;
+    bullet->y = e->y;
+    bullet->health = 1;
+    bullet->texture = alienBulletTexture;
+    bullet->side = e->side;
+    SDL_QueryTexture(bullet->texture, NULL, NULL, &bullet->w, &bullet->h);
+
+    bullet->x += (e->w / 2) - (bullet->w / 2);
+    bullet->y += (e->h / 2) - (bullet->h / 2);
+
+    calcSlope(player->x + (player->w / 2), player->y + (player->h / 2), e->x, e->y, &bullet->dx, &bullet->dy);
+
+    bullet->dx *= ALIEN_BULLET_SPEED;
+    bullet->dy *= ALIEN_BULLET_SPEED;
+
+    bullet->side = SIDE_ALIEN;
+
+    e->reload = (rand() % FPS * 2);
+}
+
+static void clipPlayer(void)
+{
+    if (player != NULL)
+    {
+        if (player->x < 0)
+        {
+            player->x = 0;
+        }
+
+        if (player->y < 0)
+        {
+            player->y = 0;
+        }
+
+        if (player->x > SCREEN_WIDTH / 2)
+        {
+            player->x = SCREEN_WIDTH / 2;
+        }
+
+        if (player->y > SCREEN_HEIGHT - player->h)
+        {
+            player->y = SCREEN_HEIGHT - player->h;
+        }
     }
 }
